384772853d35114a5aea2392f188317119c84fff
Erwan Keribin <erwan.keribin@gmail.com>
2020-04-20 01:00:58 +0200

    Add reconversion mechanism

--- app/City.gd
+++ app/City.gd
@@ -3,6 +3,7 @@ extends StaticBody2D
 signal converted
 
 export var influence = 400
+export var reconvert_rate = 15
 var curr_influence
 var converted = false
 
@@ -13,7 +14,9 @@ func _ready():
 
 func _converted():
     converted = true
-    $InfluenceLabel.text = "Convert City"
+    $NameLabel.set("custom_colors/font_color", Color("#010acc"))
+    $InfluenceLabel.set("custom_colors/font_color", Color("#010acc"))
+    $InfluenceLabel.text = curr_influence as String
     emit_signal("converted")
 
 func entered(followers):
@@ -21,7 +24,22 @@ func entered(followers):
     if not(converted):
         curr_influence -= followers
         if curr_influence < 0:
+            curr_influence *= -1
             _converted()
         else:
             $InfluenceLabel.text = curr_influence as String
     return ret
+
+func _on_ReconvertTimer_timeout():
+    if converted:
+        curr_influence -= reconvert_rate
+        if curr_influence  < 0:
+            curr_influence  = 0
+            converted = false
+            $NameLabel.set("custom_colors/font_color", Color("#fefa00"))
+            $InfluenceLabel.set("custom_colors/font_color", Color("#fefa00"))
+    else:
+        if curr_influence < influence:
+            curr_influence += reconvert_rate
+            curr_influence = min(curr_influence, influence)
+    $InfluenceLabel.text = curr_influence as String
